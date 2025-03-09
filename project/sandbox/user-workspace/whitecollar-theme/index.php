a1bfaf7a9528e2751b4c2a360cc8d370389e95e2
rommel escorihuela <rommelescorihuela@gmail.com>
2025-03-09 14:30:06 -0400

    Add project/sandbox/user-workspace/whitecollar-theme/index.php 1741545004789

---
+++ project/sandbox/user-workspace/whitecollar-theme/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<div class="main-content">
+    <h1>Welcome to WhiteCollar</h1>
+    <p>The Best Value for Business with Perfect Growth</p>
+</div>
+
+<?php get_footer(); ?>
